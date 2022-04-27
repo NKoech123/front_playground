d409e411df0e0a50c46c19fbcacbf0b085b0dbae
Nkoech123 <nicholask320@gmail.com>
2022-04-26 22:37:28 -0700

    commit

--- Languages/Typescript/ObjectTypes/object.js
+++ Languages/Typescript/ObjectTypes/object.js
@@ -1,5 +1,17 @@
 var person = {
     name: 'Nick',
-    age: 25
+    age: 25,
+    hobbies: ['Sports', 'Reading', 'Writing']
 };
+var favoritesActivities = ['Sports', 'Reading'];
+var hobbies = person.hobbies;
+var count = 0;
+for (var _i = 0, hobbies_1 = hobbies; _i < hobbies_1.length; _i++) {
+    var hobby = hobbies_1[_i];
+    if (favoritesActivities.includes(hobby)) {
+        count++;
+    }
+}
+console.log(count);
 console.log(person);
+console.log(person.hobbies);
--- Languages/Typescript/ObjectTypes/object.ts
+++ Languages/Typescript/ObjectTypes/object.ts
@@ -1,6 +1,24 @@
-const person ={
+const person = {
     name: 'Nick',
-    age:25
+    age:25,
+    hobbies: ['Sports', 'Reading', 'Writing']
 };
 
+
+let favoritesActivities = ['Sports', 'Reading'];
+
+const hobbies = person.hobbies;
+let count = 0
+
+// for (let i=0; i<hobbies.length;i++) another way
+for (const hobby of hobbies){
+
+    if (favoritesActivities.includes(hobby)) {
+        count++
+
+    }
+}
+console.log(count);
+
 console.log(person);
+console.log(person.hobbies);
