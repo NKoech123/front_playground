618daadd9b4897280307e4ba123c4b0cf357c755
Nkoech123 <nicholask320@gmail.com>
2022-06-18 14:02:24 -0700

    chakra-ui

---
+++ Chakra-UI/chakra-ui/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Text, Heading } from '@chakra-ui/react'
+
+const index = () => {
+  return (
+    <div>
+    <Heading as='h1' fontSize='6xl'>
+      This is a heading
+    </Heading>
+    </div>
+  )
+}
+
+export default index
