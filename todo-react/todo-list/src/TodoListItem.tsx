d3d8b9dc54465a5b254490539865100c5315cd3e
Nkoech123 <nicholask320@gmail.com>
2022-06-22 10:55:09 -0700

    interfaces and types

--- todo-react/todo-list/src/TodoListItem.tsx
+++ todo-react/todo-list/src/TodoListItem.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-//basic react component that has a placeholder content inside a list item
 
-export const TodoListItem = () => {
+interface Todo {
+    text: String;
+    complete: boolean;
+}
+
+interface Props {
+    todo: Todo;
+}
+//basic react component that has a placeholder content inside a list item
+export const TodoListItem: React.FC<Props> = (props) => {
     return <li>content</li>;
 };
