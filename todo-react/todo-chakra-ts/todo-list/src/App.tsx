59c1c1ef2c41d24bc442d3c78d859e71cee6e7b0
Nkoech123 <nicholask320@gmail.com>
2022-07-28 20:57:23 -0700

    clean-up

--- todo-react/todo-chakra-ts/todo-list/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Heading} from '@chakra-ui/react';
-import {TodoList} from  
-
-function App() {
-  return <Heading>Todo Application</Heading>
-}
-
-export default App;
