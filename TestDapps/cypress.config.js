fbf9381959e760ed09228c502a8284adf53e84b7
Nkoech123 <nicholask320@gmail.com>
2022-08-16 13:45:40 -0700

    cypress dashboard-trial

---
+++ TestDapps/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '7s5u7d',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
