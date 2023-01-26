fd0ebb919fe97d28985f19f2f445b7c1c50791ed
NKoech123 <nicholask320@gmail.com>
2023-01-26 05:10:52 +0300

    next app setup

---
+++ hulu/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1> Let's build Hulu 2.0</h1>
+      {/* Header */}
+      {/*Navbar */}
+      {/* Results */}
+      
+    </>
+  )
+}
