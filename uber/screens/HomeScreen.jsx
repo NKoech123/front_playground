b8c9da5afd42e385ffc3662870afa2f852ca4c27
NKoech123 <nicholask320@gmail.com>
2023-01-28 17:36:32 -0800

    configure redux and add tailwind-react-native-classnames

---
+++ uber/screens/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+         <Text>HomeScreen</Text>
+    </SafeAreaView>
+   
+
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
